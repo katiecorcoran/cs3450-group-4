be12c25657ce6f7b06c8ef548f2d3cb412e1af51
Katie Corcoran <kcorcoran50@live.com>
2021-03-20 20:02:11 -0600

    Added map, removed space model, added elements to lot page, reserve space page

--- src/parkinggenie/parkingapp/models.py
+++ src/parkinggenie/parkingapp/models.py
@@ -3,9 +3,7 @@ from django.db import models
 class Lot(models.Model):
     total_spaces = models.IntegerField(default=0)
     available_spaces = models.IntegerField(default=0)
+    available_spaces_lrg = models.IntegerField(default=0)
     reserved_spaces = models.IntegerField(default=0)
-
-class Space(models.Model):
-    lot = models.ForeignKey(Lot, on_delete=models.CASCADE)
-    is_reserved = models.BooleanField(default=False)
-    cost = models.IntegerField(default=0)
+    location = models.CharField(default='', max_length=10000)
+    nickname = models.CharField(default='', max_length=10000)
--- src/parkinggenie/parkingapp/urls.py
+++ src/parkinggenie/parkingapp/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('lots/', views.lots, name='lots'),
-    path('spaces/<int:lot_id>/', views.spaces, name='spaces'),
-    path('spaces/<int:space_id>/reserve/', views.reserve_space, name='reserve_space'),
+    path('spaces/<int:lot_id>/reserve/', views.reserve_space, name='reserve_space'),
     path('lots/<int:lot_id>/', views.lot, name='lot')
 ]
