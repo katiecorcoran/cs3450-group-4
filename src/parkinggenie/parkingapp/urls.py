f06b20b3e50d6b193caac4d3975a23fabf3caa0c
Jontay Reaves <Jontaybr@gmail.com>
2021-04-05 12:18:14 -0600

    Added the sign up page where the user can create an account

--- src/parkinggenie/parkingapp/urls.py
+++ src/parkinggenie/parkingapp/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('owner/addinglot/', views.get_TotalSpaces, name='addingspace'),
     path('owner/', views.owner, name='owner'),
     path('lots/<int:lot_id>/<str:space_type>/reserve/', views.reserve_space, name='reserve_space'),
-    path('add-lot', LotCreateView.as_view(), name='add-lot')
+    path('add-lot', LotCreateView.as_view(), name='add-lot'),
+    path('signup/', views.signup, name='signup'),
+    path('signup/create/', views.create_Account, name='createaccount')
 ]
