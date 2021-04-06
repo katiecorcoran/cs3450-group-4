1daf9e359cce931ac5ea2cf8eb6ff6d4e174eeb3
Noah Hammons <noah.hammons@gmail.com>
2021-04-06 00:40:07 -0600

    Add some things to the owner page

--- src/parkinggenie/parkingapp/forms.py
+++ src/parkinggenie/parkingapp/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class TotalSpaces(forms.Form):
-    total_spaces = forms.IntegerField(label='Total spaces')
+    available_spaces = forms.IntegerField(label='available spaces (regular)')
+    available_spaces_lrg = forms.IntegerField(label='available spaces large')
+    location = forms.CharField(label='location', max_length=10000)
+    nickname = forms.CharField(label='Name of lot', max_length=10000)
